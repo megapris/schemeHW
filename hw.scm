2775923c35623528d2058a7215c32fcf27cbe74a
Priscila Ontiveros <megapris@gmail.com>
2022-04-22 13:16:08 -0400

    some more work

--- hw.scm
+++ hw.scm
@@ -1,3 +1,5 @@
+; By Priscila Ontiveros and Josh
+
 (define fruit (list 'apples 'bananas 'grapes 'oranges 'peaches))
 (define (nth l n)
     (cond
@@ -13,3 +15,21 @@
     (nthdigit (quotient d 10)(- n 1))
     )
 )
+
+(define (list-length lst)
+  (if (null? lst)
+      0
+      (+ 1 (list-length (cdr lst)))))
+
+(define (smaller-list lst1 lst2)
+  (if (or (not (list? lst1)) (not (list? lst2)))
+      #f
+      (let ((len1 (list-length lst1))
+            (len2 (list-length lst2)))
+        (cond ((< len1 len2) #t)
+
+(define (length lis)
+   (cond ((null? lis)
+          0)
+         (else
+          (+ 1 (length (cdr lis))))))
